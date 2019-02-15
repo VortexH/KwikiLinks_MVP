d05f6e0d1ff1dda4100198692bdbcaa2ccf981dc
sumin3 <suminyu@yahoo.com>
2019-02-15 18:05:38 +0000

    add Breadth First Search

--- build_url_list.py
+++ build_url_list.py
@@ -42,7 +42,84 @@ def build_list(input_str, plcontinue="", url_list=[]):
         url_list = [t.replace(' ', '_') for t in url_list]
         return(url_list)
 
+
+def bfs_shortest_path(graph, start, goal):
+    explored = []
+    queue = [[start]]
+ 
+    if start == goal:
+        return "That was easy! Start = goal"
+    while queue:
+        path = queue.pop(0)
+        node = path[-1]
+        if node not in explored:
+            neighbours = graph[node]
+            for neighbour in neighbours:
+                new_path = list(path)
+                new_path.append(neighbour)
+                queue.append(new_path)
+                if neighbour == goal:
+                    return new_path
+            explored.append(node)
+    return "So sorry, but a connecting path doesn't exist :("
+
+def fake(v):
+    d = {'a': ['new', 'new1', 'new2', 'new3'], 
+         'new': ['v1'], 
+         'new1': ['v2'], 
+         'new2': ['v3', 'v4'], 
+         'new3': ['v5', 'v6', 'v7'], 
+         'v2': ['answer1', 'ans2'],
+         'ans2': ['c1','c2', 'final']}
+    if d.get(v):
+        return (d.get(v))
+    return []
+
+def build_graph(start, end):
+    dicts = {start: build_list(start)}
+    #dicts = {start: build_list(start)}
+    found = True
+    queue = [start]
+    while found:
+        print(dicts.get(queue[0]))
+        for value in dicts.get(queue[0]):
+            print(len(value))
+            if value == end:
+                dicts[value] = []
+                found = False
+                break;
+            if value not in dicts:
+                #print("bd", dicts)
+                dicts[value] = build_list(value)
+                #print(dicts)
+                queue.append(value)
+        print(queue)
+        queue.pop(0)
+        print("end------end", queue, queue[0])
+    return dicts
+    
 if __name__ == "__main__":
-   the_list =  build_list("Cucumberasf")
-   print(*the_list, sep="\n")
-   print("Number of links {}".format(len(the_list)))
+   #the_list =  build_list("Parachute")
+   #print(*the_list, sep="\n")
+   #print("Number of links {}".format(len(the_list)))
+   print("------")
+   #working example : corcovado -- Brazil, New --- South_Korea
+   #New -- Edel_Paragliders
+   #New -- Edel_Prime_Bi
+   #works fine up to 3 degree for few links.
+   #works fine for 1 degree - all links
+   #error message none type Regurgitator == Rodrigo_de_Freitas_Lagoon
+   #url_list += [t.get('title') for t in temp if t.get('ns') == 0]
+   # error message come from case sensitive or maybe not found
+   # New -- Ben Ely
+   start = "New"
+   end = "Edel_Prime_Bi"
+   #start = "a"
+   #end = "final"
+   graph = build_graph(start, end)
+   
+   #print(len(graph))
+   n = bfs_shortest_path(graph, start, end)
+   print("ans", n)
+   #print(graph)
+
