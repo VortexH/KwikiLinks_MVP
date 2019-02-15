fe5c9ff00f4214fa4d20ca289d18690d2d0c14eb
leobyeon <386@holbertonschool.com>
2019-02-15 23:42:51 +0000

    Adding email subscription

--- web_flask/app.py
+++ web_flask/app.py
@@ -2,8 +2,12 @@
 """
 starts a Flask web application
 """
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, jsonify, abort, request
 from os import getenv
+from werkzeug.exceptions import BadRequest
+import bfs
+import json
+import MySQLdb
 
 
 app = Flask(__name__)
@@ -34,14 +38,54 @@ def static_vendor(path):
     return send_from_directory('static/vendor', path)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/', methods=['get'])
 def index():
+    print('we gettin')
     """ basic index """
     return render_template('index.html')
+
+
+@app.route('/kwiklinks', methods=['POST'])
+def post_kwiklinks():
+    """ makes a new place """
+    if request.mimetype != 'application/json':
+        return jsonify(error="Not a JSON"), 400
+    try:
+        kwik_json = request.get_json()
+        response = bfs.BFS(kwik_json["firstw"], kwik_json["lastw"])
+    except BadRequest:
+        return jsonify(error="Not a JSON"), 400
+
+    print(kwik_json)
+
+    return response
+
+
+@app.route('/emailsubscribe', methods=['POST'])
+def post_email():
+    """ makes a new place """
+    if request.mimetype != 'application/json':
+        return jsonify(error="Not a JSON"), 400
+    try:
+        email_json = request.get_json()
+    except BadRequest:
+        return jsonify(error="Not a JSON"), 400
+    email_string = json.dumps(email_json)
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        passwd='betty2019',
+        db='user_emails')
+
+    cur = db.cursor()
+    cur.execute("INSERT INTO `emails` (email) VALUES email_string;")
+    db.close()
+    return jsonify("success")
 
 
 HOST = getenv('STINGRAY_HOST', '0.0.0.0')
 PORT = getenv('STINGRAY_PORT', '5000')
 
 if __name__ == "__main__":
-    app.run(host=HOST, port=int(PORT), threaded=True)
+app.run(host=HOST, port=int(PORT), threaded=True)
--- web_flask/static/js/app.js
+++ web_flask/static/js/app.js
@@ -1,1 +1,82 @@
 /* TODO: add event listeners for on click and close */
+//grabbing the text inputs
+let firstw = document.getElementById('firstw');
+let lastw = document.getElementById('lastw');
+
+//grabbing the button
+let sendMessageButton = document.getElementById('sendMessageButton');
+
+
+let setLoading = function () {
+  let search = $('#sendMessageButton');
+  if (!search.data('normal-text')) {
+    search.data('normal-text', search.html());
+  }
+  search.html(search.data('loading-text'));
+};
+
+let clearLoading = function () {
+  let search = $('#sendMessageButton');
+  search.html(search.data('normal-text'));
+};
+
+
+sendMessageButton.onclick = function (el) {
+  console.log(el);
+  console.log(firstw.value);
+  console.log(lastw.value);
+
+  let data = {
+    firstw: firstw.value,
+    lastw: lastw.value
+  }
+  sendMessageButton.setAttribute("disabled", "disabled");
+  setLoading();
+
+  fetch("/kwiklinks", {
+    method: "POST",
+    headers: new Headers({ 'content-type': 'application/json' }),
+    body: JSON.stringify(data)
+  }).then(function (response) {
+    // catch return from server promise, pullout json
+    return response.json();
+  }).then(function (myJson) {
+    // append results to ul so that we know servers do things
+    console.log(JSON.stringify(myJson));
+    let resList = document.getElementById('results-ul');
+    //foreach does a loop through the response json from the server, appending each to the ul
+    Object.keys(myJson).forEach(function (item, key) {
+      console.log(key);
+      console.log(item);
+      console.log(myJson[item]);
+      let li = document.createElement("li");
+      li.appendChild(document.createTextNode(item + ": " + myJson[item]));
+      resList.appendChild(li);
+    });
+    clearLoading();
+    sendMessageButton.removeAttribute("disabled");
+  });
+};
+
+
+// Email Subscription
+let email = document.getElementById('inputEmail');
+let subscribeButton = document.getElementById('subscribeButton');
+
+subscribeButton.onclick = function (el) {
+  console.log(el);
+  console.log(email.value);
+
+  let data = {
+    email: email.value
+  }
+
+  fetch("/emailsubscribe", {
+    method: "POST",
+    headers: new Headers({ 'content-type': 'application/json' }),
+    body: JSON.stringify(data)
+  }).then(function (response) {
+    // catch return from server promise, pullout json
+    return response.json();
+  });
+};
